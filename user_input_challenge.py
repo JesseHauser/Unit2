a1a45d28f6bfd3c688a2f5c19a79b678bdabe6ee
Jesse <jchauser24@students.bmchs.org>
2022-01-27 09:46:58 -0600

    updated

--- user_input_challenge.py
+++ user_input_challenge.py
@@ -11,3 +11,14 @@ many copies of the previous message. (Hint: order of operations exists in Python
 Print out that many copies of the previous message on separate lines. 
 (Hint: the string "\n" is the same as pressing the ENTER button)
 '''
+from asyncore import loop
+
+
+age = input ("what is your age")
+num =  100 - int(age)
+name = input ("what is your name")
+print (f"{name} will turn 100 years old in {num} years")
+num1 = input ("give me another number")
+repeat =  num1 
+sent = print (f"{name} will turn 100 years old in {num} years")
+
